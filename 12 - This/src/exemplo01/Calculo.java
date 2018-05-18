b725c6dc824f69d684022bef158b7ea24d9fff0f
reinaldo1707 <39385804+reinaldo1707@users.noreply.github.com>
2018-05-18 15:06:16 -0300

    12

---
+++ 12 - This/src/exemplo01/Calculo.java
@@ -0,0 +1,17 @@
+package exemplo01;
+
+public class Calculo {
+
+	//Atibutos da classe
+	public double resultado = 10;
+			
+	//Método para exibir um resultado
+	public void resultado(){
+		
+		//Variavel local
+		double resultado = 5;
+		
+		//Exibir o resultado
+		System.out.println(this.resultado);
+	}
+}
---
+++ 12 - This/src/exemplo01/Principal.java
@@ -0,0 +1,11 @@
+package exemplo01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//Instanciar
+		Calculo c = new Calculo();
+		c.resultado();
+	}
+}
